5315206e98a3f1b66d77c3291ee3cfbcb26abd65
junhyuk0073 <junhyuk0073@gmail.com>
2021-09-08 15:19:04 +0900

    수정

--- src/script.js
+++ src/script.js
@@ -1,10 +1,30 @@
 // 동영상 강의에 나온 코드를 그대로 실습하세요
 // TODO : DOM으로부터 필요한 엘리먼트를 불러오세요.
-
+const nameEl = document.querySelector('#username')
+const successID = document.querySelector('.success-message')
+const failureID = document.querySelector('.failure-message')
+const firstPassword = document.querySelector('#password')
+const secondPassword = document.querySelector('#password-retype')
+const mismatchPassword = document.querySelector('.mismatch-message')
 function isMoreThan4Length(value) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+  if(value.length >= 4){
+    successID.classList.remove('hide')
+    failureID.classList.add('hide')
+  }
+else { successID.classList.add('hide')
+failureID.classList.remove('hide')
 }
-
+}
+nameEl.onkeyup = function() {
+  isMoreThan4Length(nameEl.value)
+}
 function isMatch (password1, password2) {
-  // TODO : 동영상 강의를 보고 이 함수를 완성하세요.
+  if(password1 == password2)
+  return;
+}
+firstPassword.onkeyup = function(){
+  isMatch(firstPassword.value, secondPassword.value)
+}
+secondPassword.onkeyup = function(){
+  isMatch(firstPassword.value, secondPassword.value)
 }
